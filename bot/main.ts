ac3870c989e2d1790edca1eb263a7322cf0b8866
Andrew Dunkman <adunkman@gmail.com>
2021-08-29 13:38:56 -0400

    Add no-op Slack application.

--- bot/main.ts
+++ bot/main.ts
@@ -1,1 +1,17 @@
-console.log("it works");
+import { App, LogLevel } from "@slack/bolt";
+
+const {
+  SLACK_TOKEN,
+  SLACK_SIGNING_SECRET,
+  PORT = "3000",
+} = process.env;
+
+const app = new App({
+  token: SLACK_TOKEN,
+  signingSecret: SLACK_SIGNING_SECRET,
+  logLevel: LogLevel.DEBUG,
+});
+
+(async () => {
+  await app.start(+PORT);
+})();
