e7881a0d5cfad059e910f10303acdc2e2f642d33
Andrew Dunkman <adunkman@gmail.com>
2021-08-29 21:17:33 -0400

    Add a few API responses to edit message handler.

--- bot/actions/messageEdited.ts
+++ bot/actions/messageEdited.ts
@@ -1,5 +1,32 @@
 import { AllMiddlewareArgs, MessageChangedEvent, SlackEventMiddlewareArgs } from "@slack/bolt";
+import { config } from "../triggers/triggers";
 
+/**
+ * Remove the emoji reaction from an edited message if the message no longer
+ * causes the trigger.
+ */
 export async function handleMessageEdited(event: SlackEventMiddlewareArgs<'message'> & AllMiddlewareArgs) {
-  console.log(JSON.stringify(event, null, 2));
+  const message = event.message as MessageChangedEvent;
+
+  const auth = await event.client.auth.test();
+
+  console.log(auth);
+
+  const response = await event.client.reactions.get({
+    channel: message.channel,
+    timestamp: message.ts,
+  });
+
+  if (!response.message?.reactions?.length) {
+    // This edited message has no reactions, so it can’t be one we’re looking at
+    return;
+  }
+
+  const reaction = response.message.reactions.find((reaction) => {
+    return reaction.name === config.emoji;
+  });
+
+  console.log(reaction);
+
+  //const ourReaction = reaction.users.find(user => user === )
 }
