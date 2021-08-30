77463782ffbf650474634f3766a0175416ba37ae
Andrew Dunkman <adunkman@gmail.com>
2021-08-29 21:37:50 -0400

    Use correct message ID.

--- bot/actions/messageEdited.ts
+++ bot/actions/messageEdited.ts
@@ -8,13 +8,11 @@ import { config } from "../triggers/triggers";
 export async function handleMessageEdited(event: SlackEventMiddlewareArgs<'message'> & AllMiddlewareArgs) {
   const message = event.message as MessageChangedEvent;
 
-  console.log(message);
-
   const [ auth, response ] = await Promise.all([
     event.client.auth.test(),
     event.client.reactions.get({
       channel: message.channel,
-      timestamp: message.ts,
+      timestamp: message.previous_message.ts,
       full: true,
     }),
   ])
